2b4dc8dcfc3f69ebfdf2a282f4aeb0851cc93937
Kaushik111das <Kaushik.Das3@cognizant.com>
2026-08-19 18:40:03 +0530

    Added MonitorAPI test spec

---
+++ tests/MonitorAPI.spec.ts
@@ -0,0 +1,36 @@
+import {test , expect, APIRequest, APIResponse} from "@playwright/test"
+import { constants } from "node:buffer";
+
+test("Monitor Api responses" , async ({request}) =>{
+
+
+    // test.setTimeout(0);
+
+    while(true)
+    {
+        const startTime  = Date.now();
+
+        const healthRes : APIResponse = await request.get("https://restful-booker.herokuapp.com/ping");
+        const endTime  = Date.now();
+
+        const duration  = endTime - startTime;
+        
+        // if(duration > 100)
+        if(duration > 2000)
+        {
+            throw new Error(`API response is slow ${duration}`);
+        }
+        else
+        {
+            console.log(`Total dutaion of the response is  ${duration}`);
+        }
+
+        // console.log(`Total duration of the response : ${duration}`);
+        const sts = healthRes.status();
+        console.log(`Response code from API is : ${sts}`);
+
+        expect(sts).toBe(201);
+
+        
+    }
+})
